142537c5e8e12dbb2396981bbab6dc47f15d6fef
Angela Galvan <angamezero@gmail.com>
2025-09-24 14:35:57 -0600

    feat: Creación de rutas

--- formularioMensajes/routes/web.php
+++ formularioMensajes/routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MensajeController;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/actualizar-json', function () {
+    $response = Http::get(url('/mensajes-json'));
+    
+    if ($response->successful()) {
+        Storage::put('mensajes.json', $response->body());
+        return "Archivo actualizado con éxito.";
+    } else {
+        return "Error al obtener mensajes.";
+    }
+});
+Route::get('/actualizar-mensajes-json', [App\Http\Controllers\MensajeController::class, 'actualizarMensajesJson']);
+
+Route::get('/', [MensajeController::class, 'index']); // formulario
+Route::post('/guardar', [MensajeController::class, 'guardar']); // guardar mensaje
+Route::get('/registros', [MensajeController::class, 'registros']); // mostrar mensajes
+Route::get('/mensajes-json', [MensajeController::class, 'mostrarJson']);
+
+Route::get('/inicio', function () {
+    return view('inicio');
+})->name('inicio');
+
+
+
