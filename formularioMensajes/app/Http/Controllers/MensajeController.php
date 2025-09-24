336b2821e8ae785f81607e14cbf3ec6247f45810
Angela Galvan <angamezero@gmail.com>
2025-09-24 14:17:52 -0600

    fix: Corrección de la función registros

--- formularioMensajes/app/Http/Controllers/MensajeController.php
+++ formularioMensajes/app/Http/Controllers/MensajeController.php
@@ -43,7 +43,6 @@ class MensajeController extends Controller
     public function registros()
     {
         $archivo = 'mensajes.json';
-
         if (Storage::exists($archivo)) {
             $contenido = Storage::get($archivo);
             $mensajes = json_decode($contenido, true) ?? [];
